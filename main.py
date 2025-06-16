62506105828c2c8b3635c7804650481426072221
diep-09-Nguyen <diep.n0994@gmail.com>
2025-06-16 15:32:53 -0500

    Added history for the LLM

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 env_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=env_key)
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+user_prompt = sys.argv[1]
+verbose = None
+if len(sys.argv) > 2:
+    verbose = sys.argv[2]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001", contents=messages
+)
+
+if response.text:
+    print(response.text)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        if response.usage_metadata:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
